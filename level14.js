9584d63fb92899ba7865b7658c28e17f42e34dac
fabilqis <fadillah.bilqis@gmail.com>
2018-07-11 16:06:32 +0700

    add level 15

--- level14.js
+++ level14.js
@@ -1,1 +1,15 @@
-//level 
+//level 14
+
+    var a1 = "88"
+    var b1 = "99"
+
+    let add1 = a1+ b1
+    console.log (add1)
+    let subtract1 = a1 - b1 
+    console.log (subtract1)
+    let multiply1 = a1 * b1
+    console.log (multiply1)
+    let divide1 = a1 / b1
+    console.log(divide1)
+    let modulo1 = a1 % b1
+    console.log(modulo1)
---
+++ level15.js
@@ -0,0 +1,30 @@
+// level 15
+
+let aa = 8;
+let bbb = 6;
+
+function add (aa, bbb) {
+  return aa + bbb;
+}
+
+function substract (aa, bbb) {
+    return aa - bbb;
+  }
+
+  function multiply (aa, bbb) {
+    return aa * bbb;
+  }
+
+  function divide (aa, bbb) {
+    return aa / bbb;
+  }
+
+  function modulus (aa, bbb) {
+    return aa % bbb;
+  }
+
+console.log(add (aa, bbb)); 
+console.log(substract (aa, bbb));
+console.log(multiply (aa, bbb));
+console.log(divide (aa, bbb));
+console.log(modulus (aa, bbb));
